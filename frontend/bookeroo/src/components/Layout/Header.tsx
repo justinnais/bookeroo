ab6dc33d0aa9e3e35659792e997216ed8a6e7dd4
Justin Naismith <justin.n@live.com.au>
2021-08-20 17:31:24 +1000

    Header changes with comments explaining how Components work

--- frontend/bookeroo/src/components/Layout/Header.tsx
+++ frontend/bookeroo/src/components/Layout/Header.tsx
@@ -2,162 +2,141 @@ import React from 'react';
 import {
   AppBar,
   Toolbar,
-  IconButton,
   Typography,
   Button,
   Container,
+  ButtonProps,
+  IconButton,
 } from '@material-ui/core';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
 import {
   Link as RouterLink,
   LinkProps as RouterLinkProps,
 } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
 
+/**
+ * This is the component styling - we use this to create classes that apply only to things in this component
+ * If you need to create global styles, they go in App.scss
+ * If you want to change part of the MaterialUI theme, edit the theme.tsx
+ */
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      boxShadow: 'none',
-      color: theme.palette.secondary.main,
+      boxShadow: 'none', // removes shadow from navbar
+      color: theme.palette.secondary.main, // sets text colour to main accent colour
     },
     toolbar: {
-      justifyContent: 'space-between',
+      justifyContent: 'space-between', // spaces items in navbar apart from each other
     },
-    // menuButton: {
-    //   marginRight: theme.spacing(2),
-    // },
+    menuButton: {
+      [theme.breakpoints.up('md')]: { display: 'none' }, // hide menu button on screens above medium
+    },
+    navButtons: {
+      [theme.breakpoints.down('sm')]: { display: 'none' }, // hide nav button on screens below small
+      '& *': {
+        textTransform: 'capitalize',
+      },
+    },
     title: {
       textTransform: 'uppercase',
       color: 'inherit',
       textDecoration: 'inherit',
     },
-
-    button: {
-      textTransform: 'capitalize',
-    },
   })
 );
 
+interface HeaderButtonProps extends ButtonProps {
+  text: string;
+  to?: string;
+}
+
+/**
+ * Header component located at top of web page
+ */
 export default function Header() {
   const classes = useStyles();
 
-  const HeaderButton = () => {};
-  // todo not sure about container in header
+  /* I have created these components, HeaderButton, MenuButton, NavButton and Logo
+     They are just local in this Header.tsx file as they are likely unique to this component for now
+     If I was to then use one of these outside of this file, remove it from here and make its own functional component
+  */
+
+  /**
+   * Button component that extends a regular button
+   * @param props text string, optional link string, and MUI button props
+   * @returns button
+   */
+  const HeaderButton = (props: HeaderButtonProps) => (
+    <Button
+      color='inherit'
+      component={props.to ? RouterLink : Button}
+      {...props}
+    >
+      {props.text}
+    </Button>
+  );
+
+  /**
+   * Menu button that displays on small screens, contains all links that would show on larger screens
+   */
+  const MenuButton = () => (
+    <IconButton
+      edge='start'
+      className={classes.menuButton}
+      color='inherit'
+      aria-label='menu'
+    >
+      <MenuIcon />
+    </IconButton>
+  );
+
+  /**
+   * Navigation links that show on large screens
+   */
+  const NavButtons = () => (
+    <>
+      <div className={classes.navButtons}>
+        <HeaderButton text='Search' onClick={() => console.log('search')} />
+        <HeaderButton text='Books' />
+        <HeaderButton text='Sell Books' />
+      </div>
+      <div className={classes.navButtons}>
+        <HeaderButton text='Sign In' to='/test' />
+        <HeaderButton
+          text='Sign Up'
+          variant='contained'
+          color='secondary'
+          disableElevation
+        />
+      </div>
+    </>
+  );
+
+  const Logo = () => (
+    <Typography
+      variant='h6'
+      className={classes.title}
+      component={RouterLink}
+      to='/'
+    >
+      Bookeroo
+    </Typography>
+  );
+
+  /**
+   * This is what is rendered by the component, we can see that it has the AppBar and Toolbar,
+   * which contain the Logo, NavButtons and MenuButton components we created above
+   */
   return (
     <AppBar position='static' className={classes.root}>
       <Container>
         <Toolbar className={classes.toolbar} disableGutters>
-          <div>
-            <Typography
-              variant='h6'
-              className={classes.title}
-              component={RouterLink}
-              to='/'
-            >
-              Bookeroo
-            </Typography>
-          </div>
-          <div>
-            <Button color='inherit' className={classes.button}>
-              Sign In
-            </Button>
-            <Button color='inherit' className={classes.button}>
-              Sign In
-            </Button>
-            <Button color='inherit' className={classes.button}>
-              Sign In
-            </Button>
-          </div>
-          <div>
-            <Button color='inherit' className={classes.button}>
-              Sign In
-            </Button>
-            <Button
-              variant='contained'
-              color='secondary'
-              className={classes.button}
-              disableElevation
-            >
-              Sign Up
-            </Button>
-          </div>
+          <Logo />
+          <NavButtons />
+          <MenuButton />
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
-
-// class Header extends Component {
-//   render() {
-//     return (
-//       // <Navbar expand='lg'>
-//       //   <Container>
-//       //     <Navbar.Brand href='#home'>BOOKEROO</Navbar.Brand>
-//       //     <Navbar.Toggle aria-controls='basic-navbar-nav' />
-//       //     <Navbar.Collapse
-//       //       id='basic-navbar-nav'
-//       //       className='justify-content-between'
-//       //     >
-//       //       <div></div>
-//       //       <Nav>
-//       //         <Nav.Link href='#home'>Search</Nav.Link>
-//       //         <Nav.Link href='#link'>Books</Nav.Link>
-//       //         <Nav.Link href='#link'>Sell Books</Nav.Link>
-//       //       </Nav>
-//       //       <Nav>
-//       //         <Nav.Link
-//       //           href='#link'
-//       //           className='primary'
-//       //           style={{ paddingRight: '1rem', paddingLeft: '1rem' }}
-//       //         >
-//       //           Sign In
-//       //         </Nav.Link>
-//       //         <Button>Sign Up</Button>
-//       //       </Nav>
-//       //     </Navbar.Collapse>
-//       //   </Container>
-//       // </Navbar>
-//       <div>
-//         <nav className='navbar navbar-expand-sm navbar-dark bg-primary mb-4'>
-//           <div className='container'>
-//             <a className='navbar-brand' href='Dashboard'>
-//               Person Management Tool
-//             </a>
-//             <button
-//               className='navbar-toggler'
-//               type='button'
-//               data-toggle='collapse'
-//               data-target='#mobile-nav'
-//             >
-//               <span className='navbar-toggler-icon' />
-//             </button>
-
-//             <div className='collapse navbar-collapse' id='mobile-nav'>
-//               <ul className='navbar-nav mr-auto'>
-//                 <li className='nav-item'>
-//                   <a className='nav-link' href='/dashboard'>
-//                     Dashboard
-//                   </a>
-//                 </li>
-//               </ul>
-
-//               <ul className='navbar-nav ml-auto'>
-//                 <li className='nav-item'>
-//                   <a className='nav-link ' href='register'>
-//                     Sign Up
-//                   </a>
-//                 </li>
-//                 <li className='nav-item'>
-//                   <a className='nav-link' href='login'>
-//                     Login
-//                   </a>
-//                 </li>
-//               </ul>
-//             </div>
-//           </div>
-//         </nav>
-//       </div>
-//     );
-//   }
-// }
-// export default Header;
