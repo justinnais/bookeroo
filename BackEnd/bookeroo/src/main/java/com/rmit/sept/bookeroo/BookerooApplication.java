7e9ca7a616047056a29c4985f60563407a585cc5
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-08-24 18:11:14 +1000

    Created the master project, successfully made the login service a dependancy

--- BackEnd/bookeroo/src/main/java/com/rmit/sept/bookeroo/BookerooApplication.java
+++ BackEnd/bookeroo/src/main/java/com/rmit/sept/bookeroo/BookerooApplication.java
@@ -2,9 +2,20 @@ package com.rmit.sept.bookeroo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.rmit.sept.bk_loginservices")
+@EnableJpaRepositories("com.rmit.sept.bk_loginservices")
+@EntityScan("com.rmit.sept.bk_loginservices")
 public class BookerooApplication {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookerooApplication.class, args);
