04087e4b4107168aa5c82f3bba5f012c9f233bb7
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-08-26 15:27:25 +1000

    Another step towards the wireframe Added "Sign Up" heading as well as a submit button

--- frontend/bookeroo/src/components/UserManagement/Register.tsx
+++ frontend/bookeroo/src/components/UserManagement/Register.tsx
@@ -1,11 +1,14 @@
-import { Box, createStyles, makeStyles, Paper, TextField, Theme } from '@material-ui/core';
+import { Box, Button, createStyles, makeStyles, Paper, TextField, Theme } from '@material-ui/core';
 import { Form, Formik } from 'formik';
 import React from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            background: theme.palette.primary.main,
+            background: theme.palette.primary.main
+        },
+        textField: {
+            backgroundColor: 'white'
         },
     })
 );
@@ -16,12 +19,13 @@ export default function Register() {
     return (
         <Box display="flex" justifyContent="center" alignItems="center">
             <Paper elevation={0} className={classes.root}>
+                <h1>Sign Up</h1>
                 <Formik
                     initialValues={{
 
                     }}
                     validate={(values) => {
-                        
+
                     }}
                     onSubmit={(values, { setSubmitting }) => {
 
@@ -29,45 +33,62 @@ export default function Register() {
                 >
                     {(
                         <Form>
-                            <Box>
+                            <Box margin>
                                 <TextField
                                     variant="outlined"
                                     name="fname"
                                     type="text"
                                     label="First name"
+                                    className={classes.textField}
                                 />
                             </Box>
-                            <Box>
+                            <Box margin>
                                 <TextField
                                     variant="outlined"
                                     name="lname"
                                     type="text"
                                     label="Last name"
+                                    className={classes.textField}
                                 />
                             </Box>
-                            <Box>
+                            <Box margin>
                                 <TextField
                                     variant="outlined"
                                     name="email"
                                     type="text"
                                     label="Email"
+                                    className={classes.textField}
                                 />
                             </Box>
-                            <Box>
+                            <Box margin>
                                 <TextField
                                     variant="outlined"
                                     name="password"
                                     type="password"
                                     label="Password"
+                                    className={classes.textField}
                                 />
                             </Box>
-                            <Box>
+                            <Box margin>
                                 <TextField
                                     variant="outlined"
                                     name="confPassword"
                                     type="password"
                                     label="Confirm Password"
+                                    className={classes.textField}
                                 />
+                            </Box>
+                            <a href="/">Publisher or Store Owner?</a>
+                            <Box>
+                                <Button
+                                    variant="contained"
+                                    disableElevation
+                                    color="secondary"
+                                    name="submit"
+                                    type="submit"
+                                >
+                                    Sign Up
+                                </Button>
                             </Box>
                         </Form>
                     )}
