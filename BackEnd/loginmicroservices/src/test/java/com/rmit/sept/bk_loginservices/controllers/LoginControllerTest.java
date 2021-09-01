68a45f9529bcce58dbd22cfa3b6bee067748f220
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-01 18:50:01 +1000

    Whatever Dan did worked. Integration tests now work as expected

--- BackEnd/loginmicroservices/src/test/java/com/rmit/sept/bk_loginservices/controllers/LoginControllerTest.java
+++ BackEnd/loginmicroservices/src/test/java/com/rmit/sept/bk_loginservices/controllers/LoginControllerTest.java
@@ -1,48 +1,37 @@
 package com.rmit.sept.bk_loginservices.controllers;
 
-import com.rmit.sept.bk_loginservices.model.User;
-import com.rmit.sept.bk_loginservices.utils.AccountType;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.MapBindingResult;
-import org.springframework.validation.ObjectError;
-
-import javax.validation.ConstraintViolationException;
-import java.util.HashMap;
-import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class LoginControllerTest
 {
     @Autowired
-    private LoginController loginController;
-
-    private MapBindingResult bindingResult;
-    private User user;
-
-    @BeforeEach
-    private void setup()
-    {
-        bindingResult = new MapBindingResult(new HashMap<>(), "bindingResult");
-        user = new User();
-    }
+    private MockMvc mvc;
 
     @Test
-    @DisplayName("Test should pass when constraints are violated")
-    public void CreateWithoutDetails()
+    public void test() throws Exception
     {
-        // A valid password is required to pass the validator
-        user.setPassword("qwerty");
-        user.setConfirmPassword("qwerty");
-        // Exceptions are thrown without an account type - this is a bug
-        user.setAccountType(AccountType.STANDARD);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{}");
 
-        Assertions.assertThrows(ConstraintViolationException.class, () -> loginController.registerUser(user, bindingResult));
+        MockHttpServletResponse response = mvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn().getResponse();
+
+        Assertions.assertEquals(response.getStatus(), 400);
     }
 }
