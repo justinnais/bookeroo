70b50c79f4557b23819f413e39583aed67689690
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-09-02 18:14:15 +1000

    Dockerised frontend

--- frontend/bookeroo/run-web.sh
+++ frontend/bookeroo/run-web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /opt/codedeploy-agent/bookeroo/frontend/bookeroo &&
-npm install &&
-npm start
+docker build -t bookeroo-frontend:latest . &&
+docker run -d -p 80:80 bookeroo-frontend:latest
