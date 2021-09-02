51a7600c49811793cb84ac2e02699772d4ae434d
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-09-02 17:47:48 +1000

    Added -d tag

--- BackEnd/loginmicroservices/build-run-docker.sh
+++ BackEnd/loginmicroservices/build-run-docker.sh
@@ -2,6 +2,5 @@
 
 cd /opt/codedeploy-agent/bookeroo/BackEnd/loginmicroservices &&
 mvn clean && mvn package &&
-docker login &&
 docker build -t bookeroo/login-microservice . &&
-docker run -p 8080:8080 bookeroo/login-microservice
+docker run -p -d 8080:8080 bookeroo/login-microservice
