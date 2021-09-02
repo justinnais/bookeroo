541a123f34ed093bff1311ca40fcf7c6097d465c
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-09-02 17:50:02 +1000

    Put the -d ion the correct spot

--- BackEnd/loginmicroservices/build-run-docker.sh
+++ BackEnd/loginmicroservices/build-run-docker.sh
@@ -3,4 +3,4 @@
 cd /opt/codedeploy-agent/bookeroo/BackEnd/loginmicroservices &&
 mvn clean && mvn package &&
 docker build -t bookeroo/login-microservice . &&
-docker run -p -d 8080:8080 bookeroo/login-microservice
+docker run -d -p 8080:8080 bookeroo/login-microservice
