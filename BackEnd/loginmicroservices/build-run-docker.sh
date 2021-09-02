8837866c588b4963008f9d973adfa73dc39fac69
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-09-02 17:37:34 +1000

    Updated deploy script

--- BackEnd/loginmicroservices/build-run-docker.sh
+++ BackEnd/loginmicroservices/build-run-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /bookeroo/BackEnd/loginmicroservices
-mvn clean && mvn package
-docker login
-docker build -t bookeroo/login-microservice .
+cd /bookeroo/BackEnd/loginmicroservices &&
+mvn clean && mvn package &&
+docker login &&
+docker build -t bookeroo/login-microservice . &&
 docker run -p 8080:8080 bookeroo/login-microservice
