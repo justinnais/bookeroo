f368aa049283f75fd7fa428779cddfbc401afa68
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-10 13:10:39 +1000

    Initialising transaction microservice project

---
+++ BackEnd/transactionmicroservices/src/main/java/com/rmit/sept/bk_transactionservices/transactionmicroservices/TransactionmicroservicesApplication.java
@@ -0,0 +1,15 @@
+package com.rmit.sept.bk_transactionservices.transactionmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionmicroservicesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TransactionmicroservicesApplication.class, args);
+    }
+
+}
---
+++ BackEnd/transactionmicroservices/src/test/java/com/rmit/sept/bk_transactionservices/transactionmicroservices/TransactionmicroservicesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rmit.sept.bk_transactionservices.transactionmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionmicroservicesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
