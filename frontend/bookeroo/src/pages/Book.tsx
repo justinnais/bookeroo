d05cbad7edf3bf05322f6795b69145d2633a7cc5
Justin Naismith <justin.n@live.com.au>
2021-09-11 17:24:40 +1000

    Added seller table

--- frontend/bookeroo/src/pages/Book.tsx
+++ frontend/bookeroo/src/pages/Book.tsx
@@ -14,6 +14,13 @@ import Container from "../components/Layout/Container";
 import GridLayout from "../components/Layout/GridLayout";
 import TextCard from "../components/Layout/TextCard";
 import { theme } from "../styles/theme";
+import {
+    DataGrid,
+    GridCellParams,
+    GridColDef,
+    GridValueGetterParams,
+} from "@material-ui/data-grid";
+import SellerTable from "../components/SellerTable";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -96,6 +103,64 @@ export default function Book(props: Props) {
         </div>
     );
     const secondCard = [quote, details];
+
+    const addToCartButton = (params: GridCellParams) => (
+        <Button variant="contained" color="secondary">
+            Add to cart
+        </Button>
+    );
+
+    // TODO this needs to be turned into its own component, maybe better to use regular table instead of data grid
+    // https://material-ui.com/components/tables/#table
+    const columns: GridColDef[] = [
+        { field: "id", headerName: "ID" },
+        {
+            field: "sellerName",
+            headerName: "Seller",
+            minWidth: 200,
+        },
+        {
+            field: "condition",
+            headerName: "Condition",
+            minWidth: 200,
+        },
+        {
+            field: "price",
+            headerName: "Price",
+            type: "number",
+            minWidth: 150,
+        },
+        {
+            field: "button",
+            headerName: "",
+            minWidth: 200,
+            align: "right",
+            renderCell: addToCartButton,
+        },
+    ];
+
+    const rows = [
+        { id: 1, sellerName: "Jon", price: 35, condition: "Good" },
+        { id: 2, sellerName: "Cersei", price: 42, condition: "Good" },
+        { id: 3, sellerName: "Jaime", price: 45, condition: "Good" },
+        { id: 4, sellerName: "Arya", price: 16, condition: "Good" },
+        { id: 5, sellerName: "Daenerys", price: null, condition: "Good" },
+        { id: 6, sellerName: null, price: 150, condition: "Good" },
+        { id: 7, sellerName: "Ferrara", price: 44, condition: "Good" },
+        { id: 8, sellerName: "Rossini", price: 36, condition: "Good" },
+        { id: 9, sellerName: "Harvey", price: 65, condition: "Good" },
+    ];
+
+    const table = (
+        <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                disableSelectionOnClick
+            />
+        </div>
+    );
     return (
         <div>
             <Container noMargin>
@@ -106,6 +171,10 @@ export default function Book(props: Props) {
                 noMargin
             >
                 <GridLayout items={secondCard} size={[7, 5]} spacing={2} />
+            </Container>
+            <Container>
+                <Typography variant="h4">Sellers</Typography>
+                {table}
             </Container>
         </div>
     );
