7d5b81cb177e74114e52d7a6fc0ee0af755317eb
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-13 14:04:02 +1000

    /list/{id} can now be queried to get all transactions made by a user

--- BackEnd/transmicroservices/src/main/java/com/rmit/sept/bk_transservices/transmicroservices/controller/TransController.java
+++ BackEnd/transmicroservices/src/main/java/com/rmit/sept/bk_transservices/transmicroservices/controller/TransController.java
@@ -5,12 +5,15 @@ import com.rmit.sept.bk_transservices.transmicroservices.model.Transaction;
 import com.rmit.sept.bk_transservices.transmicroservices.model.TransactionItem;
 import com.rmit.sept.bk_transservices.transmicroservices.repositories.TransactionItemRepository;
 import com.rmit.sept.bk_transservices.transmicroservices.repositories.TransactionRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -45,9 +48,22 @@ public class TransController
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping(value="/list/{buyerId}")
-    public ResponseEntity<?> list(@PathVariable("buyerId") Long buyerId, BindingResult result)
+    @GetMapping(value = "/list/{buyerId}")
+    public ResponseEntity<?> list(@PathVariable("buyerId") Long buyerId)
     {
-        return new ResponseEntity<>(HttpStatus.OK);
+        JSONArray transactions = new JSONArray();
+        List<Double[]> transItemByBuyerId = transItemRepository.getTransItemByBuyerId(buyerId);
+
+        for (Double[] detail : transItemByBuyerId)
+        {
+            JSONObject transaction = new JSONObject();
+            transaction.put("transaction_id", detail[0]);
+            transaction.put("listing_id", detail[1]);
+            transaction.put("price", detail[2]);
+
+            transactions.put(transaction);
+        }
+
+        return ResponseEntity.ok(transactions.toString());
     }
 }
