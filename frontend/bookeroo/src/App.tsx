57b5113bc173251b486ad09ab996849ab08dab2e
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-13 14:29:34 +1000

    Removed route to paypal page because it won't be used as a page

--- frontend/bookeroo/src/App.tsx
+++ frontend/bookeroo/src/App.tsx
@@ -19,7 +19,6 @@ import PrivateRoute from "./routes/PrivateRoute";
 import Profile from "./pages/Profile";
 import AdminRoute from "./routes/AdminRoute";
 import Admin from "./pages/Admin";
-import PayPal from "./pages/PayPal"
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -51,8 +50,6 @@ function App() {
                         <PrivateRoute path={Routes.Profile} component={Profile} />
                         {/* Admin Routes */}
                         <AdminRoute exact path={Routes.Admin} component={Admin} />
-
-                        <Route exact path={"/paypal"} component={PayPal} />
 
                     </Switch>
                 </div>
