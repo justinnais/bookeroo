b2484c70dce2f7c3e19542f6f3e1090b3e911896
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-13 14:21:39 +1000

    Removed test data

--- frontend/bookeroo/src/pages/PayPal.tsx
+++ frontend/bookeroo/src/pages/PayPal.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import { PayPalButton } from "react-paypal-button-v2";
-import { string } from "yup/lib/locale";
 import { post } from "../api/api";
 import { CreateTransactionRequest } from "../api/microservices/trans";
 import { Listing } from "../api/microservices/trans";
 
-export default function PayPal(listings: Array<Listing>, buyer_id: string) {
+export default function PayPal(price: string, listings: Array<Listing>, buyer_id: string) {
     return (
         <div>
             <script src="https://www.paypal.com/sdk/js?client-id=sd&currency=AUD" />
             <PayPalButton
-                amount="0.01"
+                amount={price}
                 onSuccess={() => {
-                    const listing: Listing = {
-                        listing_id: "asdf",
-                        price: "123"
-                    }
                     const request: CreateTransactionRequest = {
                         type: "trans/transaction",
-                        listings: [listing],
-                        buyer_id: "buyer_id_temp",
+                        listings: listings,
+                        buyer_id: buyer_id,
                     };
-                    console.log(request)
-                    console.log(post(request));
+                    post(request);
                 }}
             />
         </div>
