ee9fc4456dc397b37aad7133cf244f641fd97c51
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-13 15:16:54 +1000

    Creation of service for listings

---
+++ BackEnd/listingmicroservice/src/main/java/com/rmit/sept/bk_listingservice/listingmicroservice/Listingmicroservice.java
@@ -0,0 +1,13 @@
+package com.rmit.sept.bk_listingservice.listingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Listingmicroservice
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Listingmicroservice.class, args);
+    }
+}
