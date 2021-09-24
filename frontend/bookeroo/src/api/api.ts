48aae0c217a3023fd42274f26edbf349827764b3
Justin Naismith <justin.n@live.com.au>
2021-09-24 11:38:29 +1000

    added appropriate default that doesn't work because of microservices

--- frontend/bookeroo/src/api/api.ts
+++ frontend/bookeroo/src/api/api.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 import storage from "../util/storage";
 
+const token = storage.getToken();
+if (token) {
+    axios.defaults.headers["Authorization"] = `Bearer ${token}`;
+}
+
 export const api = axios.create({
     baseURL: "https://bookeroo-api.danieljmills.com/api",
 });
