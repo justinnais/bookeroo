ee4ee4c5142821802f474d15e4f89549dcc809f5
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-09-26 14:17:15 +1000

    Beginning implementation for special accounts

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/controllers/LoginController.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/controllers/LoginController.java
@@ -7,6 +7,8 @@ import com.rmit.sept.bk_loginservices.payload.LoginRequest;
 import com.rmit.sept.bk_loginservices.security.JwtTokenProvider;
 import com.rmit.sept.bk_loginservices.services.MapValidationErrorService;
 import com.rmit.sept.bk_loginservices.services.UserService;
+import com.rmit.sept.bk_loginservices.utils.AccountStatus;
+import com.rmit.sept.bk_loginservices.utils.AccountType;
 import com.rmit.sept.bk_loginservices.validator.UserValidator;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,26 +19,19 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 import static com.rmit.sept.bk_loginservices.security.SecurityConstant.TOKEN_PREFIX;
-
-import java.util.Optional;
 
 /**
  * This file contains all api calls related to user login and registration
  */
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080"}) // allows for CORS when testing locally
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080"})
+// allows for CORS when testing locally
 public class LoginController
 {
 
@@ -51,33 +46,48 @@ public class LoginController
     @Autowired
     private UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     // LIST
     @GetMapping("")
-    public ResponseEntity<?> listUsers() {
-        return new ResponseEntity<>(userService.listUsers(),HttpStatus.OK);
+    public ResponseEntity<?> listUsers()
+    {
+        return new ResponseEntity<>(userService.listUsers(), HttpStatus.OK);
     }
 
     // GET 
     @GetMapping("/{displayName}")
-    public ResponseEntity<?> getUser(@PathVariable String displayName) {
+    public ResponseEntity<?> getUser(@PathVariable String displayName)
+    {
         log.info("Get request for " + displayName);
         User user = userService.getUser(displayName);
-        if (user == null) {
-            return new ResponseEntity<>("No user exists with this display name", HttpStatus.NOT_FOUND);
+        if (user == null)
+        {
+            return new ResponseEntity<>("No user exists with this display name",
+                    HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    // REGISTER
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result)
     {
         log.info("Receiving register request");
 
+        if (user.getAccountType() == AccountType.BUSINESS)
+            user.setAccountStatus(AccountStatus.PENDING);
+        else
+            user.setAccountStatus(AccountStatus.ACTIVE);
+
         // Validate passwords match
         userValidator.validate(user, result);
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if (errorMap != null) return errorMap;
+        if (errorMap != null)
+            return errorMap;
 
         log.info("New user is valid");
 
@@ -88,21 +98,13 @@ public class LoginController
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-
-    @Autowired
-    private JwtTokenProvider tokenProvider;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-
-    // LOGIN
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,
                                               BindingResult result)
     {
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if (errorMap != null) return errorMap;
+        if (errorMap != null)
+            return errorMap;
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -114,9 +116,9 @@ public class LoginController
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
         log.info("Logged in user: " + loginRequest.getUsername());
+
         return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
     }
 
-    
 
 }
