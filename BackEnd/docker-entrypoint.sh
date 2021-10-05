352a6be6704950a4141a96cb98ef26fa8ebf756f
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-10-05 19:39:50 +1100

    Created script to test and deploy services into 1 docker

--- BackEnd/docker-entrypoint.sh
+++ BackEnd/docker-entrypoint.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-cd book
-mvn package -Dmaven.test.skip
-
-cd ../listing
-mvn package -Dmaven.test.skip
-
-cd ../login
-mvn package -Dmaven.test.skip
-
-cd ../review
-mvn package -Dmaven.test.skip
-
-cd ../trans
-mvn package -Dmaven.test.skip
-
 cd ..
-java -jar book/target/bookmicroservices-0.0.1.jar &
-java -jar listing/target/listingmicroservice-0.0.1.jar &
-java -jar login/target/loginmicroservices-0.0.1.jar &
-java -jar review/target/reviewmicroservices-0.0.1.jar &
-java -jar trans/target/transmicroservices-0.0.1.jar &
+java -jar book.jar &
+java -jar listing.jar &
+java -jar login.jar &
+java -jar review.jar &
+java -jar trans.jar &
 wait
