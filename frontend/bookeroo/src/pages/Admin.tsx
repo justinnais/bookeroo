7046b821c7c7d28bcf254900444b5bf156ac2536
Justin Naismith <justin.n@live.com.au>
2021-10-08 17:18:58 +1100

    Cleaned admin page

--- frontend/bookeroo/src/pages/Admin.tsx
+++ frontend/bookeroo/src/pages/Admin.tsx
@@ -1,85 +1,13 @@
-import {
-    createStyles,
-    Divider,
-    List,
-    ListItem,
-    ListItemText,
-    makeStyles,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Theme,
-} from "@material-ui/core";
-import { Skeleton } from "@material-ui/lab";
 import React from "react";
-import { useMutation, useQuery } from "react-query";
-import { editUser, listUsers } from "../api/stores/user";
 import Container from "../components/Layout/Container";
 import TextCard from "../components/Layout/TextCard";
 import { theme } from "../styles/theme";
-import { IAccount } from "../api/models/Account";
-import { Link as RouterLink, useHistory } from "react-router-dom";
-import { convertDate } from "./Profile";
-import GenericTable, { TableColumn } from "../components/Table/GenericTable";
-import Button from "../components/Button/Button";
-import Menu, { IMenuItem } from "../components/Layout/Menu";
-import { AccountStatus } from "../util/enums";
-import { useAlertStore } from "../stores/useAlertStore";
 import UserTable from "../components/Table/AdminTables/UserTable";
 import Tabs from "../components/Layout/Tabs";
 import BookTable from "../components/Table/AdminTables/BookTable";
 import TransactionTable from "../components/Table/AdminTables/TransactionTable";
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        content: {
-            display: "grid",
-            gridTemplateColumns: "1fr minmax(150px, 20%)",
-        },
-        list: {
-            background: theme.palette.primary.main,
-        },
-        tableRow: {
-            textDecoration: "none",
-        },
-    })
-);
-
 export default function Admin() {
-    const classes = useStyles();
-    const history = useHistory();
-
-    /* const listItems = [
-        {
-            label: "Pending accounts",
-            link: console.log("update this"),
-        },
-        {
-            label: "Account management",
-            link: console.log("update this"),
-        },
-        {
-            label: "Book management",
-            link: console.log("update this"),
-        },
-        {
-            label: "Generate user report",
-            link: console.log("update this"),
-        },
-        {
-            label: "Generate transaction report",
-            link: console.log("update this"),
-        },
-        {
-            label: "View logs",
-            link: console.log("update this"),
-        },
-    ]; */
-
     const tabs = [
         { label: "Users", component: <UserTable /> },
         { label: "Books", component: <BookTable /> },
@@ -99,21 +27,7 @@ export default function Admin() {
             </Container>
 
             <Container>
-                {/* <Paper className={classes.content}> */}
                 <Tabs tabs={tabs} />
-                {/* <List className={classes.list}>
-                        {listItems.map((item, index, arr) => {
-                            return (
-                                <div>
-                                    <ListItem button>
-                                        <ListItemText primary={item.label} />
-                                    </ListItem>
-                                    {index !== arr.length - 1 && <Divider />}
-                                </div>
-                            );
-                        })}
-                    </List> */}
-                {/* </Paper> */}
             </Container>
         </div>
     );
