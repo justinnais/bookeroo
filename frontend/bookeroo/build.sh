fa1e5735bb178744138b3de8cab7b1e942af3156
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-10-18 14:07:40 +1100

    Hopefully fixed build script

--- frontend/bookeroo/build.sh
+++ frontend/bookeroo/build.sh
@@ -2,8 +2,7 @@
 
 #if run build runs with warnings, circleci dies
 #This script intercepts that and only errors if the code is not 0 or 1
-
-npm run build
+cd frontend/bookeroo && sudo npm run build
 CODE=$?
 if [[ $CODE -gt 1 ]]
 then
