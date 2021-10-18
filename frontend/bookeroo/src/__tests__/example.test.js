a443d4da4343ca6fe05ccaa6da898a7df7328dea
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-10-18 13:34:00 +1100

    Changed test format

--- frontend/bookeroo/src/__tests__/example.test.js
+++ frontend/bookeroo/src/__tests__/example.test.js
@@ -1,8 +1,6 @@
-const assert = require('assert');
-
 //DO NOT KEEP THIS FILE, WRITE SOME REAL TESTS.
 //THIS IS JUST SO CIRCLECI WON'T ERROR OUT
 
 it('Should return 6', () => {
-    assert.equal(2*3,6);
+    expect(2*3).toEqual(6);
 });
