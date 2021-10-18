7684121db9b0258f255098a6eebd0c0c3419d4b8
DANIEL MILLS <s3843035@student.rmit.edu.au>
2021-10-18 13:32:34 +1100

    Added an example test

---
+++ frontend/bookeroo/src/__tests__/example.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+//DO NOT KEEP THIS FILE, WRITE SOME REAL TESTS.
+//THIS IS JUST SO CIRCLECI WON'T ERROR OUT
+
+it('Should return 6', () => {
+    assert.equal(2*3,6);
+});
