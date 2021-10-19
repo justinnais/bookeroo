2f6bee7317167ae05939114b6d879e0b65a5f967
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-10-19 12:26:48 +1100

    Added common logic to review test

--- BackEnd/reviewmicroservices/src/test/java/com/rmit/sept/bk_reviewservices/controllers/ReviewControllerTest.java
+++ BackEnd/reviewmicroservices/src/test/java/com/rmit/sept/bk_reviewservices/controllers/ReviewControllerTest.java
@@ -1,8 +1,61 @@
 package com.rmit.sept.bk_reviewservices.controllers;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class ReviewControllerTest
 {
+    @Autowired
+    private MockMvc mvc;
 
+    private static Connection db;
+
+    @BeforeAll
+    static void dbConnect() throws SQLException
+    {
+        // Create database connection for cleaning test users
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "admin");
+        connectionProps.put("password", "(rN9p:NdKHD:");
+        db = DriverManager.getConnection("jdbc:mysql://bookeroo-db.cy3gnqvujqx0.ap-southeast-2" +
+                ".rds.amazonaws.com:3306/bookeroo", connectionProps);
+    }
+
+    @AfterAll
+    static void dbClose() throws SQLException
+    {
+        db.close();
+    }
+
+    private MockHttpServletResponse getResponse(MockHttpServletRequestBuilder requestBuilder) throws Exception
+    {
+        return getResponse(requestBuilder, "");
+    }
+
+    private MockHttpServletResponse getResponse(MockHttpServletRequestBuilder requestBuilder,
+                                                String content) throws Exception
+    {
+        ResultActions resultActions = mvc.perform(requestBuilder.content(content));
+        resultActions.andDo(MockMvcResultHandlers.print());
+        Assertions.assertNotNull(resultActions);
+        return resultActions.andReturn().getResponse();
+    }
 }
