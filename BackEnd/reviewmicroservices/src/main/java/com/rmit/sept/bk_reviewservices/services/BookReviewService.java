b61fb0cbb00f035202a125e2be645d730d4cc341
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-10-22 21:44:08 +1100

    Repository methods now directly return

--- BackEnd/reviewmicroservices/src/main/java/com/rmit/sept/bk_reviewservices/services/BookReviewService.java
+++ BackEnd/reviewmicroservices/src/main/java/com/rmit/sept/bk_reviewservices/services/BookReviewService.java
@@ -8,8 +8,7 @@ import com.rmit.sept.bk_reviewservices.repositories.BookReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 @Service
 public class BookReviewService
@@ -17,25 +16,25 @@ public class BookReviewService
     @Autowired
     private BookReviewRepository bookReviewRepository;
 
-    public Set<BookReview> listReviewsForBook(String isbn)
+    public List<BookReview> listReviewsForBook(String isbn)
     {
-        return new HashSet<>(bookReviewRepository.findByBookIsbn(isbn));
+        return bookReviewRepository.findByBookIsbn(isbn);
     }
 
-    public Set<BookReview> listReviewsForUser(Long userId)
+    public List<BookReview> listReviewsForUser(Long userId)
     {
-        return new HashSet<>(bookReviewRepository.findByUserId(userId));
+        return bookReviewRepository.findByUserId(userId);
     }
 
     public void postReview(BookReview bookReview) throws DuplicateReviewException,
             BadReviewScoreException, BadReviewTextException
     {
-        if (bookReviewRepository.findByUserIdAndBookIsbn(bookReview.getUserId(), bookReview.getBookIsbn()) != null)
+        if (bookReviewRepository.findByUserIdAndBookIsbn(bookReview.getUserId(),
+                bookReview.getBookIsbn()) != null)
             throw new DuplicateReviewException("User has already reviewed this book");
 
         if (bookReview.getScore() > BookReview.MAX_SCORE || bookReview.getScore() < 0.0)
             throw new BadReviewScoreException("Score must be between 0.0 and 5.0");
-
 
         String reviewText = bookReview.getReview();
         if (reviewText != null && reviewText.length() > BookReview.MAX_LENGTH)
