af67fc8aaebd3961759153173b7cdb870819f7f6
Josh Mackay (s3782125) <s3782125@student.rmit.edu.au>
2021-10-22 21:44:59 +1100

    Added methods to get reviews by reviewer and reviewed

--- BackEnd/reviewmicroservices/src/main/java/com/rmit/sept/bk_reviewservices/repositories/UserReviewRepository.java
+++ BackEnd/reviewmicroservices/src/main/java/com/rmit/sept/bk_reviewservices/repositories/UserReviewRepository.java
@@ -4,7 +4,12 @@ import com.rmit.sept.bk_reviewservices.model.UserReview;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserReviewRepository extends CrudRepository<UserReview, Long>
 {
+    List<UserReview> findUserReviewsByReviewedUserId(Long reviewedUserId);
+
+    List<UserReview> findUserReviewsByReviewerUserId(Long reviewerUserId);
 }
