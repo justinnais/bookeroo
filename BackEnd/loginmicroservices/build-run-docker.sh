50be57fa3bb252d6cac96b7bc7e72be6372e02e4
Justin Naismith <justin.n@live.com.au>
2022-01-27 16:00:51 +1100

    cleanup

--- BackEnd/loginmicroservices/build-run-docker.sh
+++ BackEnd/loginmicroservices/build-run-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /opt/codedeploy-agent/bookeroo/BackEnd/loginmicroservices &&
+cd /opt/codedeploy-agent/bookeroo/backend/loginmicroservices &&
 mvn clean && mvn package &&
 docker build -t bookeroo/login-microservice . &&
 docker run -d -p 8080:8080 bookeroo/login-microservice
